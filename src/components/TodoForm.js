2ab740f110a0c50186566d95a584299970f2b049
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2020-05-20 22:30:35 -0300

    Iniciando implementação do Redux

---
+++ src/components/TodoForm.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import Input from './Input';
+
+export default class TodoForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: ''
+        }
+    }
+
+    onChangeText(text) {
+        this.setState({
+            text
+        });
+    }
+
+    onPress() {
+        console.log(this.state.text);
+    }
+
+    render() {
+        const {text} = this.state;
+
+        return (
+            <View style={styles.formContainer}>
+                <View style={styles.inputContainer}>
+                    <Input 
+                        onChangeText={text => this.onChangeText(text)}
+                        value={text}/> 
+                </View>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        onPress={() => this.onPress()} 
+                        title='Adicionar'/>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  formContainer: {
+    flexDirection: 'row'
+  },
+  inputContainer: {
+    flex: 5
+  },
+  buttonContainer: {
+    flex: 2
+  }
+});
